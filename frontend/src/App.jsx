d9ddecf82a4455a2a2c764141508ff049b64efcf
michalspoko18 <michalw@spoko.pl>
2024-08-25 22:52:48 +0200

    create login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,39 @@
 import React from "react"
+import { BrowserRouter,Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/login"
+import Register from "./pages/register"
+import Home from "./pages/home"
+import NotFound from "./pages/404"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
